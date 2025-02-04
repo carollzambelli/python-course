24701e9fd8f0801ff57c19787e8af2e16c7add68
unknown <caroll.zambelli@gmail.com>
2025-02-03 22:41:41 -0300

    solucao exercicios

--- Fundamentos2/files/python_file.py
+++ Fundamentos2/files/python_file.py
@@ -1,1 +1,5 @@
-nome = "Carolina"
+nome_importado = "Carolina"
+
+def nome_pessoa(nome:str, idade:int) -> str:
+    """Esta função é um exemplo do uso de docstring"""
+    print(f'{nome} tem {idade} anos.')
